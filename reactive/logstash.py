da8b53e6376f6832ae667ecd234b950ea44b281e
jamesbeedy <jamesbeedy@gamil.com>
2017-11-26 14:14:10 -0800

    elastic-base trials

--- reactive/logstash.py
+++ reactive/logstash.py
@@ -1,61 +1,49 @@
-from charms.reactive import set_state
-from charms.reactive import remove_state
+from charms.reactive import set_flag
+from charms.reactive import clear_flag
 from charms.reactive import when
+from charms.reactive import when_any
+from charms.reactive import context
 from charms.reactive import when_not
 from charms.reactive import when_file_changed
-from charmhelpers.core.hookenv import config
-from charmhelpers.core.hookenv import status_set
+from charmhelpers.core.hookenv import (
+    application_version_set,
+    config,
+    status_set
+)
 from charmhelpers.core import host
 from charmhelpers.core.templating import render
-from charmhelpers.fetch import configure_sources
-from charmhelpers.fetch import apt_install
-from charmhelpers.fetch import apt_update
 from charmhelpers.core.unitdata import kv
+
+from charms.layer.logstash import logstash_version
+
 
 CONF_DIR = "/etc/logstash/conf.d"
 
 
-@when_not('java.ready')
-def messaging():
-    status_set('blocked', 'Missing JRE')
+@when('endpoint.elasticsearch.host-port')
+def trigger_logstash_service_recycle():
+    clear_flag('logstash.elasticsearch.configured')
 
 
-# this is declared in the JRE provider.
-@when('java.ready')
-@when_not('logstash.installed')
-def fetch_and_install(java):
-    configure_sources()
-    apt_update()
-    apt_install('logstash', fatal=True)
-    set_state('logstash.installed')
-    status_set('active', 'logstash installed')
-
-
-@when('elasticsearch.connected')
-def trigger_logstash_service_recycle(elasticsearch):
-    remove_state('logstash.elasticsearch.configured')
-
-
-@when('logstash.installed', 'elasticsearch.available')
+@when_any('apt.installed.logstash',
+          'deb.installed.logstash')
 @when_not('logstash.elasticsearch.configured')
-def configure_logstash(elasticsearch):
-    '''Configure logstash to push data to other sources.'''
-    # Get cluster-name, host, port from the relationship object.
+def get_all_elasticsearch_nodes_or_all_master_nodes():
     cache = kv()
-    units = elasticsearch.list_unit_data()
-    if cache.get('logstash.elasticsearch'):
+    if cache.get('logstash.elasticsearch', ''):
         hosts = cache.get('logstash.elasticsearch')
     else:
         hosts = []
 
-    for unit in units:
+    nodes = context.endpoints.elasticsearch.relation_data()
+    for unit in nodes:
         host_string = "{0}:{1}".format(unit['host'], unit['port'])
         if host_string not in hosts:
             hosts.append(host_string)
 
     cache.set('logstash.elasticsearch', hosts)
-    set_state('logstash.render')
-    set_state('logstash.elasticsearch.configured')
+    set_flag('logstash.render')
+    set_flag('logstash.elasticsearch.configured')
 
 
 @when_file_changed('/etc/logstash/conf.d/legacy.conf',
@@ -64,11 +52,22 @@ def recycle_logstash_service():
     host.service_restart('logstash')
 
 
+@when_any('apt.installed.logstash',
+          'deb.installed.logstash')
+@when_not('logstash.version.available')
+def set_logstash_version_in_unit_data():
+    ls_version = logstash_version()
+    application_version_set(ls_version)
+    status_set('active',
+               'Logstash running - version {}'.format(ls_version))
+    set_flag('logstash.version.available')
+
+
 @when('logstash.render')
 def config_changed():
     render_without_context('beats.conf', '{}/beats.conf'.format(CONF_DIR))
     render_without_context('legacy.conf', '{}/legacy.conf'.format(CONF_DIR))
-    remove_state('logstash.render')
+    clear_flag('logstash.render')
 
 
 @when('client.connected')
@@ -76,14 +75,14 @@ def configure_logstash_input(client):
     '''Configure the legacy logstash clients.'''
     # Send the port data to the clients.
     client.provide_data(config('tcp_port'), config('udp_port'))
-    set_state('logstash.render')
+    set_flag('logstash.render')
 
 
 @when('beat.connected')
 def configure_filebeat_input(filebeat):
     '''Configure the logstash beat clients.'''
     filebeat.provide_data(config('beats_port'))
-    set_state('logstash.render')
+    set_flag('logstash.render')
 
 
 def render_without_context(source, target):
